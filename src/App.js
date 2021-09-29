7d717e748b2cfe563c40c5895c545136354ec593
80andAbove <shogomiyagi94@gmail.com>
2021-09-30 00:31:17 +0900

    Created navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <p>HELLO WORLD</p>
+      <Navbar />
+      <Header />
     </div>
   );
 }
