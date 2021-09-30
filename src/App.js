25e8353bd86a9123d12ddb402d0c4d392d957d98
80andAbove <shogomiyagi94@gmail.com>
2021-10-01 00:34:17 +0900

    Created about me

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './App.scss';
 import Banner from './components/Banner'
 import Navbar from './components/Navbar'
 
@@ -7,6 +8,17 @@ function App() {
     <div className="App">
       <Navbar />
       <Banner />
+      <div className="section" id="about-me">
+        <h3>About Me</h3>
+        <p className="about-area">
+          I'm Japanese but been living abroad for most of my life.
+          Started to take up coding, taken classes, did workshops, and created a few projects.
+          I'm a pretty chill guy but really keen on making people's ideas into a digital reality.
+          Feel free to look over my recent works, and I'd be happy to read your comments and suggestions via email - and 
+          forget to follow me on my social media if you like my work or just want to follow my journey...
+          or just want a friend to talk/discuss/game with
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Banner = () => {
     return (
-        <div>
-            <h1>Shogo Miyagi</h1>
-            <h1>Web Developer</h1>
+        <div className="banner section">
+            <div className="banner-name">
+                <h1>Shogo Miyagi</h1>
+            </div>
+            <div className="banner-title">
+                <h1>Web Developer</h1>
+            </div>
+            <div className="banner-section-list">
+                <ul>
+                    <li className="list-style">
+                        <h3>Intro</h3>
+                    </li>
+                </ul>
+                <ul>
+                    <li className="list-style">
+                        <h3>About Me</h3>
+                    </li>
+                </ul>
+                <ul>
+                    <li className="list-style">
+                        <h3>Recent Works</h3>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
