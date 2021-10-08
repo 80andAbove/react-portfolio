08b024e1f1ac9c1dcc55bdd002fdb91a2c35ca5f
80andAbove <shogomiyagi94@gmail.com>
2021-10-09 00:38:36 +0900

    Removed footer js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from './components/Navbar'
 import About from './shared/About'
 import Projects from './shared/Projects'
 import ContactForm from './shared/ContactForm'
-import Footer from './components/Footer'
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
       <About />
       <Projects />
       <ContactForm />
-      <Footer />
     </div>
   );
 }
