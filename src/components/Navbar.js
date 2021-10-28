f15c245e8652cb68a4d710c08a640ddd40313b1b
80andAbove <shogomiyagi94@gmail.com>
2021-10-28 17:02:26 +0900

    WIP on the name toggle color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,14 +13,29 @@ const envelopeStyle = {
     width: "50px",
 }
 
+window.onload = () => {
+    const before_switch = document.querySelector('#name-change');
+    const name = document.querySelector('#my-name')
+    before_switch.addEventListener('click', (event) => {
+        if(name){
+            name.style.color = "#FF6B50"
+            name.style.textShadow = "1px 1px 30px #FF0000, 1px 1px 1px #FF00A7"
+        } else{
+            name.style.color = "#5C73B7"
+        }
+    });    
+}
+
+
+
 const Navbar = () => {
     return (
         <nav className="navbar bg-light">
             <div className="navbar-name-section">
-                <h1>宮城翔梧</h1>
+                <h1 id="my-name">宮城翔梧</h1>
                 <label className="switch">
                   <input type="checkbox"></input>
-                  <span className="slider round"></span>
+                  <span id="name-change" className="slider round"></span>
                 </label>
             </div>
             <div className="navbar-social-list-section">
@@ -31,12 +46,12 @@ const Navbar = () => {
                         </a>
                     </li>
                     <li className="list-style" id="linkedin-icon">
-                        <a href="#" target="_blank">
+                        <a href="www.linkedin.com/in/shogo-miyagi/" target="_blank">
                             <DevIcon icon="linkedin" style={devIconStyle} viewBox="0 0 32 32" />
                         </a>
                     </li>
                     <li className="list-style" id="envelope-icon">
-                        <a href="#" target="_blank">
+                        <a href="gmail.com" target="_blank">
                             <FontAwesomeIcon icon={faEnvelope} style={envelopeStyle} viewBox="0 0 200 200" />
                         </a>
                     </li>
